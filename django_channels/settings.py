7970e57a9d1f967a6d2119957e3401cfa1feb8eb
ujjwall7 <sharmaujjwal0921@gmail.com>
2024-10-27 15:37:43 +0530

    Flaw check

--- django_channels/settings.py
+++ django_channels/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     # Apps
     'master',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- master/consumer.py
+++ master/consumer.py
@@ -14,8 +14,8 @@ class TestConsumer(WebsocketConsumer):
         self.room_name = "test_consumer"
         self.room_group_name = "test_consumer_group"
         async_to_sync(self.channel_layer.group_add)(
-            self.room_name, 
-            self.room_group_name
+            self.room_group_name,
+            self.channel_name, 
         )
         self.accept()
         self.send(text_data = json.dumps({'status' : 'connected'})) # json.dumps is used for json convert to string
@@ -32,6 +32,15 @@ class TestConsumer(WebsocketConsumer):
     def disconnect(self, *args, **kwargs):
         print('===========Disconnected===========')
         pass
+
+    def send_notification(self, event):
+        print('===============Send Notification===============')
+        # print(event)
+        print(event.get('value'))
+        data = json.loads(event.get('value'))
+        self.send(text_data=json.dumps({'payload': data}))
+        print("===============Ended===============")
+
 
 
 
