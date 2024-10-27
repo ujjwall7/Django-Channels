39261e82649c7c601178da69849ec379bd534777
ujjwall7 <sharmaujjwal0921@gmail.com>
2024-10-27 09:52:53 +0530

    Django project setup and test consumer

---
+++ django_channels/asgi.py
@@ -0,0 +1,17 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from master.consumer import *
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
+
+ws_patterns = [
+    path('ws/test/', TestConsumer.as_asgi())
+]
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket' : URLRouter(ws_patterns)
+})
---
+++ master/consumer.py
@@ -0,0 +1,44 @@
+#State bni raiti hai -> connection orinted -> yaad rakhta hai
+ 
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+import json
+
+
+
+class TestConsumer(WebsocketConsumer):
+    
+    # ws://
+    def connect(self):
+        print('==============Started================')
+        self.room_name = "test_consumer"
+        self.room_group_name = "test_consumer_group"
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name, 
+            self.room_group_name
+        )
+        self.accept()
+        self.send(text_data = json.dumps({'status' : 'connected'})) # json.dumps is used for json convert to string
+
+
+    def receive(self, text_data):
+        print(f"{text_data = }")
+        self.send(text_data=json.dumps({
+            'status' : True,
+            'msg' : 'Got Your Data'
+        }))
+        # pass
+
+    def disconnect(self, *args, **kwargs):
+        print('===========Disconnected===========')
+        pass
+
+
+
+
+
+
+
+
+
+
